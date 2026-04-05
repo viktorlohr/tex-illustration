20c3b655ceed1053c25a64a31df4dfc68c25d86f
Viktor Lohr <viktor.lohr01@gmail.com>
2026-04-05 18:00:26 +0200

    create_alle_bilder kopiert jetzt pdf in gdrive

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 - [ ] Aufgabe 39: Letzter Abschnitt von F(x) muss = 1 sein.
 
 ## 📝 to do
-<details>
-<summary><b>Liste anzeigen</b></summary>
-
 - [x] Aufgabe51.png ersetzen
 - [x] Allgemein Graustufen!
 - [x] Normalverteilung 10er Potenz konvertieren
@@ -12,8 +9,3 @@
 - [x] Normalverteilung y-Achse Beschriftung
 - [x] Restliche .pngs konvertieren
 - [x] Implementation im Skript checken
-
-</details>
-
-## 🍃 In Overleaf öffnen
-* [alle_bilder.tex](https://www.overleaf.com/docs?snip_uri=https://raw.githubusercontent.com/viktorlohr/tex-illustration/refs/heads/main/alle_bilder.tex)
--- create_alle_bilder.py
+++ create_alle_bilder.py
@@ -1,11 +1,15 @@
 import os
+import subprocess
+import sys
 
+# create alle_bilder.tex
 lines = [
     "\\documentclass{article}\n",
     "\\input{preamble}\n",
     "\\begin{document}\n",
     ]
 
+# get all images
 for filename in os.listdir('.'):
     if (
         filename.endswith('.tex') and 
@@ -18,3 +22,21 @@ lines.append("\\end{document}")
 
 with open("alle_bilder.tex", "w") as f:
     f.writelines(lines)
+
+# compile
+filename = "alle_bilder.pdf"
+try:
+    subprocess.run(["pdflatex","-interaction=nonstopmode", "alle_bilder.tex"], check=True)
+    subprocess.run(["open", filename], check=True)
+except subprocess.CalledProcessError:
+    print("LaTeX compilation failed")
+    sys.exit(1)
+
+# copy to gdrive
+dest_dir = os.path.expanduser("~/gdrive/1_nachhilfe/akademus-skript/meine-bilder")
+os.makedirs(dest_dir, exist_ok=True)
+dest_path = os.path.join(dest_dir, filename)
+subprocess.run(["cp", filename, dest_path])
+subprocess.run(["open", dest_dir])
+
+
